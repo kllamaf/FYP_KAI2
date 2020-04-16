a5b64b19fd3cd96bd5a78514c2fd0815fe9ae65c
LAM, Kai Lok <36331212+kllamaf@users.noreply.github.com>
2020-04-17 00:42:28 +0800

    script.sh
    run simulations in parallel

--- script.sh
+++ script.sh
@@ -1,9 +1,26 @@
 
-for ((i = 0; i <= 10000; i += 5));
+for ((i = 1310; i <= 10000; i += 15));
 do
-	python run_pias_dctcp.py $(echo "scale=4; $i/10000" | bc)
-	python run_pias_dctcp_oversub.py $(echo "scale=4; $i/10000" | bc)
-	python run_pias_heter.py $(echo "scale=4; $i/10000" | bc)
-	python run_pias_vl2.py $(echo "scale=4; $i/10000" | bc)
+	python run_pias_dctcp.py $(echo "scale=4; $i/10000" | bc) & 
+	python run_pias_dctcp_oversub.py $(echo "scale=4; $i/10000" | bc) &
+	python run_pias_heter.py $(echo "scale=4; $i/10000" | bc) &
+	wait
+	
+	python run_pias_vl2.py $(echo "scale=4; $i/10000" | bc) & 
+	python run_pias_dctcp.py $(echo "scale=4; ($i+5)/10000" | bc) & 
+	python run_pias_dctcp_oversub.py $(echo "scale=4; ($i+5)/10000" | bc) &
+	wait
+
+	python run_pias_heter.py $(echo "scale=4; ($i+5)/10000" | bc) &
+	python run_pias_vl2.py $(echo "scale=4; ($i+5)/10000" | bc) & 
+	python run_pias_dctcp.py $(echo "scale=4; ($i+10)/10000" | bc) & 
+	wait
+	
+	python run_pias_dctcp_oversub.py $(echo "scale=4; ($i+10)/10000" | bc) &
+	python run_pias_heter.py $(echo "scale=4; ($i+10)/10000" | bc) &
+	python run_pias_vl2.py $(echo "scale=4; ($i+10)/10000" | bc) & 
+	wait
+
+	mv g_* /mnt/hgfs/Downloads/results/
 done
 
